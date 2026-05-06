580a572994e39e708277fdebd59d9edcf5def630
Emil Rosin <SaltisRS>
2026-05-06 02:08:36 +0200

    fix run scripts to rebuild containers on sourcecode change

--- rundev.sh
+++ rundev.sh
@@ -5,4 +5,4 @@ exec infisical run \
   --projectId=9047f633-a675-497c-8ca2-1e75ffd95db9 \
   --env=dev \
   --watch \
-  -- docker compose up -d "$@"
+  -- docker compose up -d --build "$@"
--- runprod.sh
+++ runprod.sh
@@ -5,4 +5,4 @@ exec infisical run \
   --projectId=9047f633-a675-497c-8ca2-1e75ffd95db9 \
   --env=prod \
   --watch \
-  -- docker compose -f docker-compose.yml -f docker-compose.traefik.yml up -d "$@"
+  -- docker compose -f docker-compose.yml -f docker-compose.traefik.yml up -d --build "$@"
